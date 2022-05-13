762327958fcdb9b26f6102d7fb8da6f65d68ca43
Imran Hossen <imranhossen20@gmail.com>
2022-05-13 21:26:25 +0600

    data load from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,10 +8,47 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mc2fw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('groceryItem').collection('itemsQty');
+
+        app.get('/itemsQty', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+        app.get('/itemsQty/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-    res.send('Running my site')
+    res.send('Running my server side')
 })
 
 app.listen(port, () => {
-    console.log('test', port)
+    console.log('Check the Server', port)
 })
