9ca58af270b7166b363bdc93bf726023e8796f13
Imran Hossen <imranhossen20@gmail.com>
2022-05-15 01:28:13 +0600

    heroku

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mc2fw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -28,6 +24,7 @@ async function run() {
             res.send(items);
         })
 
+        // data load
         app.get('/itemsQty/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
